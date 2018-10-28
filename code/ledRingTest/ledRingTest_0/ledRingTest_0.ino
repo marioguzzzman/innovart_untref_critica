a41dfb23739be737e9aa4a6ca059bfac33059a21
PleekMan <agusanzorena@gmail.com>
2018-10-28 17:59:05 -0300

    Mas Modos de Interfaz: SeleccionBinaria, EscaleraHasta5Puntos, CicloDeColor

--- code/ledRingTest/ledRingTest_0/ledRingTest_0.ino
+++ code/ledRingTest/ledRingTest_0/ledRingTest_0.ino
@@ -26,14 +26,14 @@ void setup() {
     //colores[i] = RgbwColor( 0, 20 , 0, 0 );
   }
 
-  leds.Begin();
+  leds.Begin(); // ALGUNA VECES TARDA EN COMENZAR A MOSTRAR. SI LE TOCO ALGUN CABLE, REVIVE.
   leds.Show();
+
 }
 
 void loop() {
 
   if (digitalRead(modeButtonPin) == HIGH)selectMode();
-
 
   runMode(atMode);
 
@@ -75,13 +75,13 @@ void arcoIrisControl() {
   int offset = potControl % 4096;
   float offsetNorm = offset / 4096.0;
 
-    Serial.println("-|| CONTROL");
-    Serial.print("RAW: ");
-    Serial.println(potControl);
-    Serial.print("OFFSET INT: ");
-    Serial.println(offset);
-    Serial.print("OFFSET NORM: ");
-    Serial.println(offsetNorm);
+  Serial.println("----------------|| CONTROL");
+  Serial.print("RAW: ");
+  Serial.println(potControl);
+  Serial.print("OFFSET INT: ");
+  Serial.println(offset);
+  Serial.print("OFFSET NORM: ");
+  Serial.println(offsetNorm);
 
 
   for (uint8_t i = 0; i < pixelCount; i++ ) {
@@ -93,17 +93,98 @@ void arcoIrisControl() {
 
 }
 
+void plainColorCycler() {
+
+  int potControl = analogRead(36); // ADCgroup_1, 12-bit (0 -> 4095) /// (ADCgroup2) cannot be use when using wiFi.
+  float selectedColor = potControl / 4096.0;
+
+  Serial.println("----------------|| PLAIN COLOR CONTROL");
+  Serial.print("RAW: ");
+  Serial.println(potControl);
+  Serial.print("FINAL COLOR: ");
+  Serial.println(selectedColor);
+
+  for (uint8_t i = 0; i < pixelCount; i++ ) {
+    leds.SetPixelColor(i, HsbColor(selectedColor, colores[i].S, colores[i].B));
+  }
+
+  leds.Show();
+
+}
+
+void binarySelect() {
+
+  int potControl = analogRead(36); // ADCgroup_1, 12-bit (0 -> 4095) /// (ADCgroup2) cannot be use when using wiFi.
+  float selectionColor = potControl < 2048 ? 1.0 : 0.3; // ROJO / VERDE
+
+  // SET ALL PIXELS BLACK
+  for (uint8_t i = 0; i < pixelCount; i++ ) {
+    leds.SetPixelColor(i, HsbColor(0, 0, 0));
+  }
+
+  // ASSIGN SELECTION COLOR TO RESPECTIVE HALF OF LED RING
+  if (selectionColor < 0.9) { // IF IT'S GREEN/YES
+    for (uint8_t i = 8; i < pixelCount; i++ ) {
+      leds.SetPixelColor(i, HsbColor(selectionColor, 1, 1));
+    }
+  } else {
+    for (uint8_t i = 0; i <= 7; i++ ) {
+      leds.SetPixelColor(i, HsbColor(selectionColor, 1, 1));
+    }
+  }
+
+  leds.Show();
+}
+
+void selectionStair() {
+
+  int potControl = analogRead(36); // ADCgroup_1, 12-bit (0 -> 4095) /// (ADCgroup2) cannot be use when using wiFi.
+  int oneToFive = ((potControl / 4096.0) * 5) + 1;
+
+  Serial.println("----------------|| SELECTION STAIR");
+  //Serial.print("RAW: ");
+  //Serial.println(potControl);
+  Serial.print("OneToFive: ");
+  Serial.println(oneToFive);
+
+  // SET ALL PIXELS BLACK
+  for (uint8_t i = 0; i < pixelCount; i++ ) {
+    leds.SetPixelColor(i, HsbColor(0, 0, 0));
+  }
+
+  // DRAW STAIR RANKING
+  for (uint8_t i = 0; i < oneToFive * 3; i++ ) {
+    leds.SetPixelColor(i, HsbColor(0.5, 1, 0.2));
+  }
+
+  // DRAW FIRST PIXEL BRIGHTER
+  leds.SetPixelColor(oneToFive * 3, HsbColor(0.8, 1, 1));
+
+
+  leds.Show();
+
+}
+
 void runMode(int m) {
 
   switch (m) {
     case 0:
-      arcoIrisRotando();
+      selectionStair();
       break;
     case 1:
-      off();
+      plainColorCycler();
       break;
     case 2:
+      binarySelect();
+      break;
+    case 3:
       arcoIrisControl();
+      break;
+    case 4:
+      arcoIrisRotando();
+      break;
+    case 5:
+      off();
       break;
     default:
       break;
@@ -111,7 +192,7 @@ void runMode(int m) {
 }
 
 void selectMode() {
-  atMode = (atMode + 1) % 3;
-  delay(200);
+  atMode = (atMode + 1) % 6;
+  delay(250);
 }
 
